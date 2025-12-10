42b295dac93f63a7f20271b2d1a7e4f5fbac3eef
gabrieljohansenthomas-ui <gabrieljohansenthomas@gmail.com>
2025-12-10 11:02:49 +0800

    Update Firebase configuration details

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAF_-NLJRCn-pDrfwsKM1JL3oBvJ176iGU",
-  authDomain: "smartlib-0710.firebaseapp.com",
-  projectId: "smartlib-0710",
-  storageBucket: "smartlib-0710.firebasestorage.app",
-  messagingSenderId: "668732924028",
-  appId: "1:668732924028:web:b6e39bb4485a863d4729f9",
-  measurementId: "G-0J93YGQD91"
+  apiKey: "AIzaSyB1shl4UA7gO6E5STC04Ljj5EnkTXpaV4U",
+  authDomain: "smartlibfinal.firebaseapp.com",
+  projectId: "smartlibfinal",
+  storageBucket: "smartlibfinal.firebasestorage.app",
+  messagingSenderId: "1008233910842",
+  appId: "1:1008233910842:web:8f55cef0e9ba78de529b03",
+  measurementId: "G-9LJ4QCLWQM"
 };
 firebase.initializeApp(firebaseConfig);
